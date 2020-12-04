c0d39303c9fc700fb8112b46545604259acb895b
Vincent-glitch <66323103+Vincent-glitch@users.noreply.github.com>
2020-12-04 14:43:40 -0500

    cleaner

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Python scripts analyzing company financial records, and a script for helping a small, rural town modernize its vote counting process.',
+    author='Vincent D'Alto',
+    license='MIT',
+)
